f77404001fd7004900f65e05d774db54db1a763f
naotojedrey <alexnaoto.jedrey.u2@flux-g.com>
2026-04-27 17:53:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ else:
     elif model_provider == "azure-openai":
         llm_stream = AzureChatOpenAI(
             azure_endpoint=os.getenv("AZ_OPENAI_ENDPOINT"),
-            openai_api_version="2024-02-15-preview",
+            openai_api_version="2025-01-01-preview",
             model_name=st.session_state.model.split("/")[-1],
             openai_api_key=os.getenv("AZ_OPENAI_API_KEY"),
             openai_api_type="azure",
